8afd2fd2fbba7b5460262ae539668f2a0301d7be
Andrei Shpakovskiy <50966785+ashpakouski@users.noreply.github.com>
2024-09-03 20:24:50 +0300

    Try to extract all embedded jpegs

--- app/src/main/java/com/shpak/easyruler/MainActivity.kt
+++ app/src/main/java/com/shpak/easyruler/MainActivity.kt
@@ -1,47 +1,55 @@
 package com.shpak.easyruler
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.shpak.easyruler.ui.theme.EasyRulerTheme
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContent {
-            EasyRulerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+        extractAllEmbeddedJpegs(assets.open("test.jpg"), "$filesDir")
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun extractAllEmbeddedJpegs(inputStream: InputStream, outputDirPath: String) {
+    val fileBytes = inputStream.readBytes()
+    inputStream.close()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EasyRulerTheme {
-        Greeting("Android")
+    val jpegMarkers = mutableListOf<Pair<Int, Int>>()
+
+    var startMarker = -1
+    for (i in fileBytes.indices) {
+        // Looking for FFD8 marker (Start of JPEG)
+        if (fileBytes[i] == 0xFF.toByte() && fileBytes[i + 1] == 0xD8.toByte()) {
+            startMarker = i
+        }
+
+        // Looking for FFD9 marker (End of JPEG)
+        if (fileBytes[i] == 0xFF.toByte() && fileBytes[i + 1] == 0xD9.toByte()) {
+            if (startMarker != -1) {
+                // Found a complete JPEG segment
+                jpegMarkers.add(Pair(startMarker, i + 2))
+                startMarker = -1 // Reset for the next JPEG
+            }
+        }
+    }
+
+    for ((index, markers) in jpegMarkers.withIndex()) {
+        val (start, end) = markers
+        val embeddedJpegBytes = fileBytes.copyOfRange(start, end)
+
+        val outputFile = File(outputDirPath, "extracted_image_$index.jpg")
+        FileOutputStream(outputFile).use { fos ->
+            fos.write(embeddedJpegBytes)
+            fos.flush()
+        }
     }
 }
