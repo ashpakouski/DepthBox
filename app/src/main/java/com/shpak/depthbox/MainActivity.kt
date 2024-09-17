adc19cd342f7d7af23a7b2b448e44e86076200e4
Andrei Shpakovskiy <50966785+ashpakouski@users.noreply.github.com>
2024-09-17 20:39:45 +0300

    Apply test depth effect

--- app/src/main/java/com/shpak/depthbox/MainActivity.kt
+++ app/src/main/java/com/shpak/depthbox/MainActivity.kt
@@ -11,17 +11,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.boundsInParent
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.sp
 import com.shpak.depthbox.data.repository.TestDepthImageRepository
 import kotlinx.coroutines.runBlocking
 
@@ -29,19 +35,37 @@ private val depthMapShader = RuntimeShader(
     """
     uniform shader inputTexture;
     uniform shader depthMap;
-    uniform float coordMultiplier;
+    uniform shader originalPicture;
+
+    uniform float depthCoordMultiplier;
+    uniform float originalCoordMultiplier;
+
+    uniform vec4 testViewRect;
 
     vec4 main(vec2 fragCoord) {
-        vec4 depthPixel = depthMap.eval(fragCoord * coordMultiplier);
-        vec4 originalPixel = inputTexture.eval(fragCoord);
-        
+        vec4 depthPixel = depthMap.eval(fragCoord * depthCoordMultiplier);
+        vec4 originalPixel = originalPicture.eval(fragCoord * originalCoordMultiplier);
+        vec4 viewPixel = inputTexture.eval(fragCoord);
+
         float depth = depthPixel.r;
-        
-        if (depthPixel.r < 0.2) {
-            return depthPixel;
+
+        if (fragCoord.x >= testViewRect.x &&
+            fragCoord.x <= testViewRect.z &&
+            fragCoord.y >= testViewRect.y &&
+            fragCoord.y <= testViewRect.w
+        ) {
+            if (depthPixel.r < 0.15) {
+                return originalPixel;
+            } else {
+                return viewPixel;
+            }
         }
 
-        return originalPixel;
+//        if (depthPixel.r < 0.2) {
+//            return originalPixel;
+//        }
+
+        return viewPixel;
     }
     """
 )
@@ -62,17 +86,29 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             var resolution by remember { mutableStateOf(IntSize.Zero) }
+            var testViewRect by remember { mutableStateOf(Rect.Zero) }
 
             Box(modifier = Modifier.fillMaxSize()) {
-                Image(
-                    bitmap = originalBitmap.asImageBitmap(),
-                    contentDescription = null,
+                Box(
                     modifier = Modifier
                         .align(Alignment.Center)
                         .graphicsLayer {
                             depthMapShader.setFloatUniform(
-                                "coordMultiplier",
+                                "depthCoordMultiplier",
                                 depthBitmap.width / resolution.width.toFloat()
+                            )
+
+                            depthMapShader.setFloatUniform(
+                                "originalCoordMultiplier",
+                                originalBitmap.width / resolution.width.toFloat()
+                            )
+
+                            depthMapShader.setFloatUniform(
+                                "testViewRect",
+                                testViewRect.left,
+                                testViewRect.top,
+                                testViewRect.right,
+                                testViewRect.bottom
                             )
 
                             depthMapShader.setInputBuffer(
@@ -84,14 +120,42 @@ class MainActivity : ComponentActivity() {
                                 )
                             )
 
+                            depthMapShader.setInputBuffer(
+                                "originalPicture",
+                                BitmapShader(
+                                    originalBitmap,
+                                    Shader.TileMode.DECAL,
+                                    Shader.TileMode.DECAL
+                                )
+                            )
+
                             renderEffect = RenderEffect
                                 .createRuntimeShaderEffect(depthMapShader, "inputTexture")
                                 .asComposeRenderEffect()
                         }
-                        .onGloballyPositioned {
-                            resolution = it.size
-                        }
-                )
+                ) {
+                    Image(
+                        bitmap = originalBitmap.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .onGloballyPositioned {
+                                resolution = it.size
+                            }
+                    )
+
+                    Text(
+                        text = "HELLO",
+                        color = Color.Yellow,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 142.sp,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .onGloballyPositioned {
+                                testViewRect = it.boundsInParent()
+                            }
+                    )
+                }
             }
         }
     }
