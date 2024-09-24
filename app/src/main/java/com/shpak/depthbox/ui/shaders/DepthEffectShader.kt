82aa7139af8679c9707155661dcf3a58e4b694aa
Andrei Shpakovskiy <50966785+ashpakouski@users.noreply.github.com>
2024-09-24 19:50:23 +0300

    Combine pixel translations in a single function

--- app/src/main/java/com/shpak/depthbox/ui/shaders/DepthEffectShader.kt
+++ app/src/main/java/com/shpak/depthbox/ui/shaders/DepthEffectShader.kt
@@ -18,49 +18,29 @@ val DepthEffectShader = """
     uniform vec2 pictureSize;
     uniform vec2 depthMapSize;
     
-    vec2 translatePixelCoordPortrait(vec2 contentSize, vec2 viewSize, vec2 fragCoord) {
-        float viewAspectRatio = viewSize.x / viewSize.y;
-        float contentAspectRatio = contentSize.x / contentSize.y;
-        
-        float contentCoordMultiplierX =  contentSize.y / viewSize.y;
-        float contentOffsetX = (contentSize.x - viewSize.x * contentCoordMultiplierX) / 2.0;
-
-        return vec2(
-            fragCoord.x * contentCoordMultiplierX + contentOffsetX,
-            fragCoord.y * contentCoordMultiplierX
-        );
+    vec2 translatePixelCoord(vec2 contentSize, vec2 viewSize, vec2 fragCoord, bool isPortrait) {
+        float coordMultiplier = isPortrait ? (contentSize.y / viewSize.y) : (contentSize.x / viewSize.x);
+    
+        vec2 offset = isPortrait 
+            ? vec2((contentSize.x - viewSize.x * coordMultiplier) / 2.0, 0.0) 
+            : vec2(0.0, (contentSize.y - viewSize.y * coordMultiplier) / 2.0);
+            
+        return fragCoord * coordMultiplier + offset;
     }
     
-    vec2 translatePixelCoordLandscape(vec2 contentSize, vec2 viewSize, vec2 fragCoord) {
-        float contentCoordMultiplierY = contentSize.x / viewSize.x;
-        float contentOffsetY = (contentSize.y - viewSize.y * contentCoordMultiplierY) / 2.0;
-
-        return vec2(
-            fragCoord.x * contentCoordMultiplierY,
-            fragCoord.y * contentCoordMultiplierY + contentOffsetY
-        );
-    }
-
     vec4 main(vec2 fragCoord) {
-        vec4 inputPixel = inputTexture.eval(fragCoord);
-
         float viewportAspectRatio = viewportSize.x / viewportSize.y;
         float pictureAspectRatio = pictureSize.x / pictureSize.y;
-        
-        vec2 pictureCoordTranslated;
-        vec2 depthCoordTranslated;
-
-        if (viewportAspectRatio < pictureAspectRatio) {
-            pictureCoordTranslated = translatePixelCoordPortrait(pictureSize, viewportSize, fragCoord);
-            depthCoordTranslated = translatePixelCoordPortrait(depthMapSize, viewportSize, fragCoord);            
-        } else {
-            pictureCoordTranslated = translatePixelCoordLandscape(pictureSize, viewportSize, fragCoord);
-            depthCoordTranslated = translatePixelCoordLandscape(depthMapSize, viewportSize, fragCoord);  
-        }
-
+    
+        bool isPortrait = viewportAspectRatio < pictureAspectRatio;
+    
+        vec2 pictureCoordTranslated = translatePixelCoord(pictureSize, viewportSize, fragCoord, isPortrait);
+        vec2 depthCoordTranslated = translatePixelCoord(depthMapSize, viewportSize, fragCoord, isPortrait);
+    
+        vec4 inputPixel = inputTexture.eval(fragCoord);
         vec4 picturePixel = picture.eval(pictureCoordTranslated);
         vec4 depthPixel = depthMap.eval(depthCoordTranslated);
-        
+    
         if (depthPixel.x > 0.65 && inputPixel.a != 0.0) {
             return inputTexture.eval(fragCoord);
         } else {
