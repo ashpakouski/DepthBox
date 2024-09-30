4385e08e8b4375c1dbd3425d64ffd0339f700930
Andrei Shpakovskiy <50966785+ashpakouski@users.noreply.github.com>
2024-09-30 22:16:56 +0300

    Move bitmap extraction to a separate method

--- app/src/main/java/com/shpak/depthbox/data/repository/depth_image/GoogleCameraDepthImageRepository.kt
+++ app/src/main/java/com/shpak/depthbox/data/repository/depth_image/GoogleCameraDepthImageRepository.kt
@@ -1,7 +1,7 @@
 package com.shpak.depthbox.data.repository.depth_image
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
 import com.shpak.depthbox.data.model.DepthImage
 import com.shpak.depthbox.data.model.GoogleCameraXmpDirectoryStruct
 import com.shpak.depthbox.data.repository.xmp_directory.XmpDirectoryRepository
@@ -24,21 +24,24 @@ class GoogleCameraDepthImageRepository(
             val jpegs = JpegExtractor.extractEmbeddedJpegs(fileBytes)
             val xmpDirectories = xmpDirectoryRepository.extractXmpDirectoryStructs(fileBytes)
 
-            val originalImage = xmpDirectories.firstOrNull {
-                it.semantic == ORIGINAL_IMAGE_SEMANTIC_NAME
-            }?.length?.let { originalImageLength ->
-                jpegs.minByOrNull { (it.size - originalImageLength).absoluteValue }
-            } ?: throw Exception("Failed to find original image")
-
-            val depthImage = xmpDirectories.firstOrNull {
-                it.semantic == DEPTH_IMAGE_SEMANTIC_NAME
-            }?.length?.let { depthImageLength ->
-                jpegs.minByOrNull { (it.size - depthImageLength).absoluteValue }
-            } ?: throw Exception("Failed to find depth image")
-
             DepthImage(
-                main = BitmapFactory.decodeByteArray(originalImage, 0, originalImage.size),
-                depth = BitmapFactory.decodeByteArray(depthImage, 0, depthImage.size),
+                main = findBitmap(jpegs, xmpDirectories, ORIGINAL_IMAGE_SEMANTIC_NAME),
+                depth = findBitmap(jpegs, xmpDirectories, DEPTH_IMAGE_SEMANTIC_NAME)
             )
         }
+
+    private fun findBitmap(
+        jpegs: List<ByteArray>,
+        xmpDirectories: List<GoogleCameraXmpDirectoryStruct>,
+        directorySemantic: String
+    ): Bitmap {
+        val image = xmpDirectories.firstOrNull {
+            it.semantic == directorySemantic
+        }?.length?.let { imageLength ->
+            // There should be a better way, but I still can't find it
+            jpegs.minByOrNull { (it.size - imageLength).absoluteValue }
+        } ?: throw Exception("Failed to find image for: $directorySemantic")
+
+        return BitmapFactory.decodeByteArray(image, 0, image.size)
+    }
 }
